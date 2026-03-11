1d2652168ec63301edb6afc652fe279b84f5a0eb
Khunthynne <khunthynne@gmail.com>
2026-03-11 20:28:53 +0700

    chore: pre update

--- src/shared/services/chrome/index.ts
+++ src/shared/services/chrome/index.ts
@@ -1,17 +1,17 @@
 import { sendMessageToTab, sendMessage } from "./sendMessageServices";
 import { createMessageListenerService } from "./createMessageListenerService";
-
+const isExtension = typeof chrome !== "undefined" && !!chrome.runtime;
 export const chromeService = {
-  ...chrome,
+  ...(isExtension ? chrome : {}),
   tabs: {
-    ...chrome.tabs,
+    ...(isExtension ? chrome.tabs : {}),
     sendMessage: sendMessageToTab,
   },
   runtime: {
-    ...chrome.runtime,
+    ...(isExtension ? chrome.runtime : {}),
     sendMessage: sendMessage,
     onMessage: {
-      ...chrome.runtime.onMessage,
+      ...(isExtension ? chrome.runtime.onMessage : {}),
       addListenerService: createMessageListenerService,
     },
   },
